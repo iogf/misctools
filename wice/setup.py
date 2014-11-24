ce5fbc76d8b26554d29b76eb02712eb2c7a350cb
Iury O. G. Figueiredo <ioliveira.id.uff.br>
2014-11-24 16:39:11 -0200

    Adding a print_usage to wice/setup.py.

--- wice/setup.py
+++ wice/setup.py
@@ -1,20 +1,9 @@
 #! /usr/bin/env python
 
 from distutils.core import setup
-setup(name="into",
+setup(name="wice",
       version="0.1",
-      scripts=['into'],
+      scripts=['wice'],
       author="Iury O. G. Figueiredo",
       author_email="ioliveira@id.uff.br")
-
-
-
-
-
-
-
-
-
-
-
 
